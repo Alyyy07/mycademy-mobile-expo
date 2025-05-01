e5f0112354ea84f3f158a06d07236fcd49426ae3
Alyyy07 <alielhamied1@gmail.com>
2025-05-01 20:55:23 +0700

    [FIX] add mesage parameter

--- components/NoResult.tsx
+++ components/NoResult.tsx
@@ -2,12 +2,12 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { images } from '@/constants/image'
 
-const NoResult = () => {
+const NoResult = ({message}:{message?:string}) => {
   return (
     <View className='flex items-center my-5'>
       <Image source={images.noResult} style={{ width: '90%', height: 320 }} resizeMode='contain' />
       <Text className='text-2xl font-rubik-bold text-white mt-5'>Tidak ada hasil</Text>
-      <Text className='text-base text-white mt-2'>Tidak ada matakuliah yang ditemukan</Text>
+      <Text className='text-base text-white mt-2'>{message ?? "Tidak ada data yang ditemukan"}</Text>
     </View>
   )
 }
