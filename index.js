4bad4e41884f4978782af398323823ba2ba6d096
Alyyy07 <alielhamied1@gmail.com>
2025-05-06 21:51:43 +0700

    [FIX] add ignoreLog for warning in library react-native-render html

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-import { registerRootComponent } from 'expo';
+import { LogBox } from "react-native";
 
-import App from './App';
+LogBox.ignoreLogs([
+  "MemoizedTNodeRenderer: Support for defaultProps",
+  "TNodeChildrenRenderer: Support for defaultProps",
+  "TRenderEngineProvider: Support for defaultProps",
+  "IMGElement: Support for defaultProps",
+  "Support for defaultProps"
+]);
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (
+    typeof args[0] === "string" &&
+    args[0].includes("Support for defaultProps")
+  ) {
+    return;
+  }
+  originalWarn(...args);
+};
+
+import App from "./App"; // baru import App
+import { registerRootComponent } from "expo";
+
 registerRootComponent(App);
